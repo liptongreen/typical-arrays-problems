cc08477ae71451b27ca626243c8f686099488b2f
Andrey Kasperovich <liptongreen@outlook.com>
2021-03-14 16:01:30 +0300

    init: add typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-
-exports.min = function min (array) {
-  return 0;
+let required=()=>{return 0};
+exports.min = function min (array = required()) {
+  if (array==0){
+    return 0;
+  }else{
+    return Math.min.apply(null, array);
+  }
+  ;
+}
+let re=()=>{return 0};
+exports.max = function max (array = re()) {
+  if (array==[]||array==Infinity){
+    return 0;
+  }else{
+    return Math.max.apply(null, array);
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = required()) {
+  let a=0;
+  if (array!=[]||array!=NaN){
+    for(i=0;i<array.length;i++){
+      a=array[i]+a;
+    };
+    return a/array.length;
+  }else{
+    return 0;
+  }
 }
